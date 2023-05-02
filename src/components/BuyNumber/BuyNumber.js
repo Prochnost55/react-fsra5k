15640b928f155fb37e854141030e97448cd08788
Prochnost55 <aks3555@gmail.com>
2023-05-02 22:08:15 +0530

    2 files modified, 7 deleted

--- src/components/BuyNumber/BuyNumber.js
+++ src/components/BuyNumber/BuyNumber.js
@@ -4,36 +4,41 @@ import Dropdown from '../Dropdown/Dropdown';
 import { Button, Container, Row, Col } from 'react-bootstrap';
 
 const BuyNumber = (props) => {
-    const { selectedNumbers } = props;
-    // console.log('asdf=>', selectedNumbers);
-    return (
-        <Col className="d-flex justify-content-center flex-column"> 
-            <Row>Please Select a numbers to buy</Row>
-            {selectedNumbers.map((eachSelectedNumber, idx) => {
-                return (
-                    <Dropdown
-                        key={'number-dropdown-' + idx}
-                        idx={idx}
-                        data={props.availableNumbers}
-                        handleNumberSelect={props.handleNumberSelect}
-                        defaultValue={eachSelectedNumber}
-                    />
-                );
-            })}
+  const { selectedNumbers } = props;
+  return (
+    <Row className="d-flex justify-content-center flex-column">
+      <Col>Please Select a numbers to buy</Col>
+      {selectedNumbers.map((eachSelectedNumber, idx) => {
+        return (
+          <Col className="mt-1">
+            <Dropdown
+              key={'number-dropdown-' + idx}
+              idx={idx}
+              data={props.availableNumbers}
+              handleNumberSelect={props.handleNumberSelect}
+              defaultValue={eachSelectedNumber}
+            />
+          </Col>
+        );
+      })}
 
-            <Row>
-                <Col lg='6'>
-                {props.selectedNumbers?.length >= MAX_NUMBER_BUY_LIMIT ? (
-                    <div className='mt-4 text-center'>You can only buy {MAX_NUMBER_BUY_LIMIT} numbers</div>
-                ) : (
-                    <Button className="mt-4 float-right" variant="outline-primary" onClick={props.handleBuyMore}>
-                        + Buy another Number
-                    </Button>
-                )}
-                </Col>
-            </Row>
-        </Col>
-    );
+      <Col lg="6">
+        {props.selectedNumbers?.length >= MAX_NUMBER_BUY_LIMIT ? (
+          <div className="mt-4 text-center">
+            You can only buy {MAX_NUMBER_BUY_LIMIT} numbers
+          </div>
+        ) : (
+          <Button
+            className="mt-4 float-right"
+            variant="outline-primary"
+            onClick={props.handleBuyMore}
+          >
+            + Buy another Number
+          </Button>
+        )}
+      </Col>
+    </Row>
+  );
 };
 
 export default BuyNumber;
--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -2,45 +2,41 @@ import React from 'react';
 import { Form, Row, Col } from 'react-bootstrap';
 
 const Dropdown = (props) => {
-    const { data, handleNumberSelect, defaultValue, idx } = props;
-    const handleOptionClick = (e) => {
-        let id = e.target.value;
+  const { data, handleNumberSelect, defaultValue, idx } = props;
+  const handleOptionClick = (e) => {
+    let id = e.target.value;
 
-        handleNumberSelect(idx, id, defaultValue.id);
-    };
+    handleNumberSelect(idx, id, defaultValue.id);
+  };
 
-    return (
-        <Row className="p-2">
-            <Col lg="6">
-                <Form.Group>
-                    <Form.Label>Pick a number:</Form.Label>
-                    <Form.Select
-                        name="numbers"
-                        onChange={handleOptionClick}
-                        defaultValue={defaultValue.number}
-                    >
-                        <option key={'number-default'} value="">
-                            Select a number
-                        </option>
+  return (
+    <Form.Group>
+      <Form.Label>Pick a number:</Form.Label>
+      <Form.Select
+        name="numbers"
+        onChange={handleOptionClick}
+        defaultValue={defaultValue.number}
+      >
+        <option key={'number-default'} value="">
+          Select a number
+        </option>
 
-                        {data.map((eachData, i) => {
-                            if (
-                                eachData.number != defaultValue.number &&
-                                eachData.isSelected == true
-                            ) {
-                                return false;
-                            }
-                            return (
-                                <option key={'number-' + i} value={eachData.id}>
-                                    {eachData.number}
-                                </option>
-                            );
-                        })}
-                    </Form.Select>
-                </Form.Group>
-               </Col>
-        </Row>
-    );
+        {data.map((eachData, i) => {
+          if (
+            eachData.number != defaultValue.number &&
+            eachData.isSelected == true
+          ) {
+            return false;
+          }
+          return (
+            <option key={'number-' + i} value={eachData.id}>
+              {eachData.number}
+            </option>
+          );
+        })}
+      </Form.Select>
+    </Form.Group>
+  );
 };
 
 export default Dropdown;
